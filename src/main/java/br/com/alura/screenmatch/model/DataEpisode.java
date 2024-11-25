c4229eac5d44f922fb5840cfcf756b409401a721
luis-domingues <luisdomingues117@gmail.com>
2024-11-25 14:08:47 -0300

    feat: adicionar classes auxiliar na busca

--- src/main/java/br/com/alura/screenmatch/model/DataEpisode.java
+++ src/main/java/br/com/alura/screenmatch/model/DataEpisode.java
@@ -1,2 +1,11 @@
-package br.com.alura.screenmatch.model;public record DataEpisode() {
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DataEpisode(@JsonAlias("Title") String title,
+                          @JsonAlias("Episode") Integer episodeNumber,
+                          @JsonAlias("imdbRating") String rating,
+                          @JsonAlias("Released") String releaseDate){
 }
--- src/main/java/br/com/alura/screenmatch/model/Episode.java
+++ src/main/java/br/com/alura/screenmatch/model/Episode.java
@@ -1,2 +1,80 @@
-package br.com.alura.screenmatch.model;public class Episode {
+package br.com.alura.screenmatch.model;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class Episode {
+    private Integer season;
+    private String title;
+    private Integer episodeNumber;
+    private Double rating;
+    private LocalDate releaseDate;
+
+    public Episode(Integer season, DataEpisode dataEpisode) {
+        this.season = season;
+        this.title = dataEpisode.title();
+        this.episodeNumber = dataEpisode.episodeNumber();
+
+        try {
+            this.rating = Double.valueOf(dataEpisode.rating());
+        } catch (NumberFormatException ex) {
+            this.rating = 0.0;
+        }
+
+        try {
+            this.releaseDate = LocalDate.parse(dataEpisode.releaseDate());
+        } catch (DateTimeParseException ex) {
+            this.releaseDate = null;
+        }
+
+    }
+
+    public Integer getSeason() {
+        return season;
+    }
+
+    public void setSeason(Integer season) {
+        this.season = season;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getEpisodeNumber() {
+        return episodeNumber;
+    }
+
+    public void setEpisodeNumber(Integer episodeNumber) {
+        this.episodeNumber = episodeNumber;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    @Override
+    public String toString() {
+        return "season=" + season +
+                ", title='" + title + '\'' +
+                ", episodeNumber=" + episodeNumber +
+                ", rating=" + rating +
+                ", releaseDate=" + releaseDate;
+    }
 }
